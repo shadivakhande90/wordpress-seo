65e2d4771c44bca39a78a166386d81b061bd7193
Igor <35524806+igorschoester@users.noreply.github.com>
2023-04-21 10:31:26 +0200

    Run storybook upgrade
    * Upgrade Babel along with it and enforce @babel/core to 7.21.4 in yarn.lock or it breaks
    * Replace @storybook/addon-postcss with @storybook/addon-styling
    * Add @storybook/blocks in preparation for mdx fixing

--- packages/ui-library/.storybook/main.js
+++ packages/ui-library/.storybook/main.js
@@ -12,19 +12,23 @@ module.exports = {
 		"@storybook/addon-essentials",
 		"@storybook/addon-a11y",
 		{
-			name: "@storybook/addon-postcss",
+			name: "@storybook/addon-styling",
 			options: {
-				postcssLoaderOptions: {
+				postCss: {
 					// Provide our own copy of PostCSS.
 					implementation: require( "postcss" ),
 				},
 			},
 		},
 	],
+	framework: {
+		name: "@storybook/react-webpack5",
+		options: {},
+	},
 	core: {
 		disableTelemetry: true,
 	},
-	features: {
-		previewMdx2: true,
+	docs: {
+		autodocs: true,
 	},
 };
