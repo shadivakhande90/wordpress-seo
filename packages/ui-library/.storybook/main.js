07583298f1351cd76634da141bcdbcfc080b9f35
Igor <35524806+igorschoester@users.noreply.github.com>
2023-04-21 15:06:06 +0200

    Fix markdown in mdx

--- packages/ui-library/.storybook/main.js
+++ packages/ui-library/.storybook/main.js
@@ -1,9 +1,5 @@
 module.exports = {
 	stories: [
-		"../src/introduction.stories.mdx",
-		"../src/installation.stories.mdx",
-		"../src/contributing.stories.mdx",
-		"../src/changelog.stories.mdx",
 		"../src/**/*.stories.@(js|mdx)",
 		"../src/**/stories.@(js|mdx)",
 	],
